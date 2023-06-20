3fa040571735da8e110ba2831e5742f3ffa63167
100nam <shanuyadav1812@gmail>
2023-06-20 14:46:11 +0530

    dump to sonam

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -57,6 +57,5 @@ class Test_001_Login:
             self.driver.save_screenshot(".\\screenshots\\" + "test_login.png")
             self.driver.close()
             self.logger.error("**************** Test case is failed *********************")
-            self.logger.error("**************** Test case is failed *********************")
 
             assert False
